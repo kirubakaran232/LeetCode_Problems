c2adb90e92b18fac3cdae05126f763e0e4fcea1a
Kirubakaran <165383562+kirubakaran232@users.noreply.github.com>
2026-05-25 19:15:59 +0530

    Create 1402-CountSquareSubmatricesWithAllOnes

---
+++ 1402-CountSquareSubmatricesWithAllOnes/1402-CountSquareSubmatricesWithAllOnes.java
@@ -0,0 +1,16 @@
+// Last updated: 5/25/2026, 7:15:58 PM
+class Solution {
+    public int countSquares(int[][] mat) {
+        int dp[][] = new int[mat.length+1][mat[0].length+1];
+        int c = 0;
+        for(int i=1;i<dp.length;i++){
+            for(int j=1;j<dp[0].length;j++){
+                if(mat[i-1][j-1]==1){
+                    dp[i][j] = 1 + Math.min(Math.min(dp[i-1][j], dp[i][j-1]),dp[i-1][j-1]);
+                }
+                c+=dp[i][j];
+            }
+        }
+        return c;
+    }
+}
