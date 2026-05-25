a5a34729d4e2fed255e26d3cf5421163d668434e
Kirubakaran <165383562+kirubakaran232@users.noreply.github.com>
2026-05-25 19:20:39 +0530

    Create 221-MaximalSquare

---
+++ 221-MaximalSquare/221-MaximalSquare.java
@@ -0,0 +1,18 @@
+// Last updated: 5/25/2026, 7:20:39 PM
+class Solution {
+    public int maximalSquare(char[][] matrix) {
+        int rows = matrix.length;
+        int cols = matrix[0].length;
+        int max = 0;
+        int [][] dp = new int [rows+1][cols+1];
+        for (int i = 1; i <= rows; i++) {
+            for (int j = 1; j <= cols; j++) {
+                if (matrix[i-1][j-1] == '1') {
+                    dp[i][j] = 1 + Math.min(Math.min(dp[i-1][j], dp[i][j-1]),dp[i-1][j-1]);
+                }
+                max = Math.max(dp[i][j], max);
+            }
+        }
+        return max * max;
+    }
+}
