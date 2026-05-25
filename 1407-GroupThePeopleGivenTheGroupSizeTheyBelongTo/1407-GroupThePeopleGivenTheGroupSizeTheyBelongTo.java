2490255d50d0c56b844dfc4fe3b1c4181d0ee5b2
Kirubakaran <165383562+kirubakaran232@users.noreply.github.com>
2026-05-25 19:15:55 +0530

    Create 1407-GroupThePeopleGivenTheGroupSizeTheyBelongTo

---
+++ 1407-GroupThePeopleGivenTheGroupSizeTheyBelongTo/1407-GroupThePeopleGivenTheGroupSizeTheyBelongTo.java
@@ -0,0 +1,17 @@
+// Last updated: 5/25/2026, 7:15:54 PM
+class Solution {
+    public List<List<Integer>> groupThePeople(int[] groupSizes) {
+        List<List<Integer>> res = new ArrayList<>();
+        Map<Integer, List<Integer>> groups = new HashMap<>();
+        for (int i = 0; i < groupSizes.length; i++) {
+            int size = groupSizes[i];
+            groups.putIfAbsent(size, new ArrayList<>());
+            groups.get(size).add(i);
+            if (groups.get(size).size() == size) {
+                res.add(groups.get(size));
+                groups.remove(size);
+            }
+        }
+        return res;
+    }
+}
