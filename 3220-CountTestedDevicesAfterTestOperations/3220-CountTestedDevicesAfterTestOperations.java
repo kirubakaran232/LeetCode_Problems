b79340ab38ffb7de79e2c1168ff85e273bda4ee6
Kirubakaran <165383562+kirubakaran232@users.noreply.github.com>
2026-05-25 19:09:02 +0530

    Create 3220-CountTestedDevicesAfterTestOperations

---
+++ 3220-CountTestedDevicesAfterTestOperations/3220-CountTestedDevicesAfterTestOperations.java
@@ -0,0 +1,15 @@
+// Last updated: 5/25/2026, 7:09:02 PM
+class Solution {
+    public int countTestedDevices(int[] batteryPercentages) {
+        int count=0;
+        for(int i=0;i<batteryPercentages.length;i++){
+            if(batteryPercentages[i]>0){
+                count++;
+                for(int j=i+1;j<batteryPercentages.length;j++){
+                    batteryPercentages[j] = Math.max(0, batteryPercentages[j] - 1);
+                }
+            }
+        }
+        return count;
+    }
+}
