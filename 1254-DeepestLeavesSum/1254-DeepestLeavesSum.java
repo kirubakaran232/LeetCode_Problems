9bbb8b9ee6070caee8ab250c67822fd74f964794
Kirubakaran <165383562+kirubakaran232@users.noreply.github.com>
2026-05-25 19:16:25 +0530

    Create 1254-DeepestLeavesSum

---
+++ 1254-DeepestLeavesSum/1254-DeepestLeavesSum.java
@@ -0,0 +1,38 @@
+// Last updated: 5/25/2026, 7:16:25 PM
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public int deepestLeavesSum(TreeNode root) {
+        Queue<TreeNode> q = new LinkedList<>();
+        q.add(root);
+        int sum = 0;
+        while(!q.isEmpty()){
+            int size = q.size();
+            sum = 0;
+            for(int i=0;i<size;i++){
+                TreeNode curr = q.poll();
+                sum+=curr.val;
+                if(curr.left!=null){
+                    q.add(curr.left);
+                }
+                if(curr.right!=null){
+                    q.add(curr.right);
+                }
+            }
+        }
+        return sum;
+    }
+}
