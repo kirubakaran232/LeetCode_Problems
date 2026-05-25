b3951558da044ad0cc36adcfcc9e98148b84af52
Kirubakaran <165383562+kirubakaran232@users.noreply.github.com>
2026-05-25 19:09:40 +0530

    Create 2857-TotalDistanceTraveled

---
+++ 2857-TotalDistanceTraveled/2857-TotalDistanceTraveled.java
@@ -0,0 +1,6 @@
+// Last updated: 5/25/2026, 7:09:40 PM
+class Solution {
+    public int distanceTraveled(int mainTank, int additionalTank) {
+        return (mainTank + Math.min((mainTank - 1) / 4, additionalTank)) * 10;
+    }
+}
