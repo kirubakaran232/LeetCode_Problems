8eba228cd40d53de6f371e27c5a52c2920ce99f6
Kirubakaran <165383562+kirubakaran232@users.noreply.github.com>
2026-05-25 19:10:57 +0530

    Create 2519-FindTheOriginalArrayOfPrefixXor

---
+++ 2519-FindTheOriginalArrayOfPrefixXor/2519-FindTheOriginalArrayOfPrefixXor.java
@@ -0,0 +1,11 @@
+// Last updated: 5/25/2026, 7:10:56 PM
+class Solution {
+    public int[] findArray(int[] pref) {
+        int[] arr = new int[pref.length];
+        arr[0] = pref[0];
+        for (int i = 1; i < pref.length; i++) {
+            arr[i] = pref[i] ^ pref[i - 1];
+        }
+        return arr;
+    }
+}
