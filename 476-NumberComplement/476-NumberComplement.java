3ea3ec9654ec701fbea3dd71c22c62cff1373c3c
Kirubakaran <165383562+kirubakaran232@users.noreply.github.com>
2026-05-25 19:19:09 +0530

    Create 476-NumberComplement

---
+++ 476-NumberComplement/476-NumberComplement.java
@@ -0,0 +1,16 @@
+// Last updated: 5/25/2026, 7:19:08 PM
+class Solution {
+    public int findComplement(int num) {
+        String str = Integer.toBinaryString(num);
+        String res = "";
+        for(int i=0;i<str.length();i++){
+            if(str.charAt(i) == '0'){
+                res += '1';
+            }
+            else{
+                res += '0';
+            }
+        }
+        return Integer.parseInt(res,2);
+    }
+}
