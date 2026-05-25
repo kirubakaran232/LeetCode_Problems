dc98c29c5abaa8247be8d28b155bf13e8ec91db0
Kirubakaran <165383562+kirubakaran232@users.noreply.github.com>
2026-05-25 19:11:48 +0530

    Create 2298-CountIntegersWithEvenDigitSum

---
+++ 2298-CountIntegersWithEvenDigitSum/2298-CountIntegersWithEvenDigitSum.java
@@ -0,0 +1,18 @@
+// Last updated: 5/25/2026, 7:11:47 PM
+class Solution {
+ public static int countEven(int num) {
+    	int count = 0;
+    	 for (int i = 1; i <= num; i++) {
+             int sum = 0;
+             int n = i;
+             while (n > 0) {
+                 sum += n % 10;
+                 n /= 10;
+             }
+             if (sum % 2 == 0) {
+                 count++;
+             }
+         }
+         return count;
+     }
+}
