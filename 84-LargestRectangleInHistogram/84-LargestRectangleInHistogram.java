4af9b84cafb1e9ad64ecda6748d1eaeda6ef187b
Kirubakaran <165383562+kirubakaran232@users.noreply.github.com>
2026-05-25 19:22:21 +0530

    Create 84-LargestRectangleInHistogram

---
+++ 84-LargestRectangleInHistogram/84-LargestRectangleInHistogram.java
@@ -0,0 +1,24 @@
+// Last updated: 5/25/2026, 7:22:20 PM
+class Solution {
+    public int largestRectangleArea(int[] heights) {
+        Stack<Integer> st = new Stack<>();
+        int length = heights.length;
+        int area = 0;
+        for (int i = 0; i < length; i++) {
+            while (!st.empty() && heights[i] < heights[st.peek()]) {
+                int bar = st.pop(); 
+                int pse = st.empty() ? -1 : st.peek(); 
+                int nse = i;
+                area = Math.max(area, heights[bar] * (nse - pse - 1));
+            }
+            st.push(i);
+        }
+        while (!st.empty()) {
+            int bar = st.pop();
+            int pse = st.empty() ? -1 : st.peek();
+            int nse = length;
+            area = Math.max(area, heights[bar] * (nse - pse - 1));
+        }
+        return area;
+    }
+}
