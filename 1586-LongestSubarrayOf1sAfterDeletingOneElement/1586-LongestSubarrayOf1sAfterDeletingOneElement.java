e4e5a4509dc458ff3525c0545040a13b410421ee
Kirubakaran <165383562+kirubakaran232@users.noreply.github.com>
2026-05-25 19:14:53 +0530

    Create 1586-LongestSubarrayOf1sAfterDeletingOneElement

---
+++ 1586-LongestSubarrayOf1sAfterDeletingOneElement/1586-LongestSubarrayOf1sAfterDeletingOneElement.java
@@ -0,0 +1,17 @@
+// Last updated: 5/25/2026, 7:14:53 PM
+class Solution {
+    public int longestSubarray(int[] nums) {
+        int left = 0;
+        int max_length = 0;
+        int last_zero = -1;
+        for (int right = 0; right < nums.length; right++) {
+            if (nums[right] == 0) {
+                left = last_zero + 1;
+                last_zero = right;
+            }
+            max_length = Math.max(max_length, right - left);
+        }
+
+        return max_length;        
+    }
+}
