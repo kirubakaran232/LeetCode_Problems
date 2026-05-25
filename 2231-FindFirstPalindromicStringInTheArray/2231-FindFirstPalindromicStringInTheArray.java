8cf922d98e51161a487b6b56ddd9b3aaf7aa193a
Kirubakaran <165383562+kirubakaran232@users.noreply.github.com>
2026-05-25 19:12:16 +0530

    Create 2231-FindFirstPalindromicStringInTheArray

---
+++ 2231-FindFirstPalindromicStringInTheArray/2231-FindFirstPalindromicStringInTheArray.java
@@ -0,0 +1,12 @@
+// Last updated: 5/25/2026, 7:12:15 PM
+public class Solution {
+    public static String firstPalindrome(String[] words) {
+        for (String word : words) {
+            StringBuilder reversed = new StringBuilder(word).reverse();
+            if (word.equals(reversed.toString())) {
+                return word;
+            }
+        }
+        return "";
+    }
+}
