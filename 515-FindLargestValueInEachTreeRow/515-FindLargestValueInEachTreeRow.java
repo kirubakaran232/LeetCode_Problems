4ab1f4b6421a473c1a5d0f8874f940f32754a6ca
Kirubakaran <165383562+kirubakaran232@users.noreply.github.com>
2026-05-25 19:18:59 +0530

    Create 515-FindLargestValueInEachTreeRow

---
+++ 515-FindLargestValueInEachTreeRow/515-FindLargestValueInEachTreeRow.java
@@ -0,0 +1,40 @@
+// Last updated: 5/25/2026, 7:18:58 PM
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public List<Integer> largestValues(TreeNode root) {
+        if (root == null) return new ArrayList<>();
+        Queue<TreeNode> q = new LinkedList<>();
+        List<Integer> l = new ArrayList<>();
+        q.add(root);
+        while(!q.isEmpty()){
+            int size = q.size();
+            int max = Integer.MIN_VALUE;
+            for(int i=0;i<size;i++){
+                TreeNode curr = q.poll();
+                max=Math.max(curr.val,max);
+                if(curr.left!=null){
+                    q.add(curr.left);
+                }
+                if(curr.right!=null){
+                    q.add(curr.right);
+                }
+            }
+            l.add(max);
+        }
+        return l;
+    }
+}
