93c89f6d71f1c303f597bd2318f3711499a08459
Kirubakaran <165383562+kirubakaran232@users.noreply.github.com>
2026-05-25 19:23:17 +0530

    Create 34-FindFirstAndLastPositionOfElementInSortedArray

---
+++ 34-FindFirstAndLastPositionOfElementInSortedArray/34-FindFirstAndLastPositionOfElementInSortedArray.java
@@ -0,0 +1,15 @@
+// Last updated: 5/25/2026, 7:23:17 PM
+class Solution {
+    public int[] searchRange(int[] nums, int target) {
+        int[] res = {-1, -1};
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] == target) {
+                if (res[0] == -1) {
+                    res[0] = i;   
+                }
+                res[1] = i;       
+            }
+        }
+        return res;
+    }
+}
