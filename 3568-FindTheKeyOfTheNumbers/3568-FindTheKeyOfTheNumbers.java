8c5b2f1b55441d92b0bd850f0defa30e256e55fc
Kirubakaran <165383562+kirubakaran232@users.noreply.github.com>
2026-05-25 19:07:56 +0530

    Create 3568-FindTheKeyOfTheNumbers

---
+++ 3568-FindTheKeyOfTheNumbers/3568-FindTheKeyOfTheNumbers.java
@@ -0,0 +1,16 @@
+// Last updated: 5/25/2026, 7:07:55 PM
+class Solution {
+    public int generateKey(int num1, int num2, int num3) {
+        int ans = 0, x = 1;
+        while(num1 > 0 || num2 > 0 || num3 > 0) {
+            int min_digit = Math.min((num1 % 10) , (num2 % 10));
+            min_digit = Math.min((num3 % 10) , min_digit);
+            ans += x * min_digit;
+            x *= 10;
+            num1 /= 10;
+            num2 /= 10;
+            num3 /= 10;
+        }
+        return ans;
+    }
+}
