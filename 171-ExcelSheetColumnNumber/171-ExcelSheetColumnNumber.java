d6d5a22660fd73d965a47d4bb829c7699f225eff
Kirubakaran <165383562+kirubakaran232@users.noreply.github.com>
2026-05-25 19:21:20 +0530

    Create 171-ExcelSheetColumnNumber

---
+++ 171-ExcelSheetColumnNumber/171-ExcelSheetColumnNumber.java
@@ -0,0 +1,12 @@
+// Last updated: 5/25/2026, 7:21:20 PM
+public class Solution {
+    public int titleToNumber(String s) {
+        if (s == null) return -1;
+        int sum = 0;
+        for (char c : s.toUpperCase().toCharArray()) {
+            sum *= 26;
+            sum += c - 'A' + 1;
+        }
+        return sum;
+    }
+}
