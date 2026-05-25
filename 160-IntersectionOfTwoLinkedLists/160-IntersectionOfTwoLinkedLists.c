923b2180a30840ab8d9e65763cf948f011e1c3a6
Kirubakaran <165383562+kirubakaran232@users.noreply.github.com>
2026-05-25 19:21:35 +0530

    Create 160-IntersectionOfTwoLinkedLists

---
+++ 160-IntersectionOfTwoLinkedLists/160-IntersectionOfTwoLinkedLists.c
@@ -0,0 +1,20 @@
+// Last updated: 5/25/2026, 7:21:34 PM
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     struct ListNode *next;
+ * };
+ */
+struct ListNode *getIntersectionNode(struct ListNode *headA, struct ListNode *headB) {
+    if (headA == NULL || headB == NULL) {
+        return NULL;
+    }
+    struct ListNode *a = headA;
+    struct ListNode *b = headB;
+    while (a != b) {
+        a = (a == NULL) ? headB : a->next;
+        b = (b == NULL) ? headA : b->next;
+    }
+    return a;
+}
