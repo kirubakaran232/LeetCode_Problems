3fc6fef3849a00dcc148675a66b200d3ccb8e94e
Kirubakaran <165383562+kirubakaran232@users.noreply.github.com>
2026-05-25 19:16:41 +0530

    Create 1184-CarPooling

---
+++ 1184-CarPooling/1184-CarPooling.java
@@ -0,0 +1,22 @@
+// Last updated: 5/25/2026, 7:16:40 PM
+class Solution {
+    public boolean carPooling(int[][] trips, int capacity) {
+        int[] diff = new int[1001];
+        for (int[] trip : trips) {
+            int passengers = trip[0];
+            int from = trip[1];
+            int to = trip[2];
+            diff[from] += passengers;   
+            diff[to] -= passengers;
+        }
+        int currentPassengers = 0;
+        for (int i = 0; i < diff.length; i++) {
+            currentPassengers += diff[i];
+            if (currentPassengers > capacity) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
