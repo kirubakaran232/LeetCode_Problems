64fb57a343ac21debdf5236b71e12c8c7d923d42
Kirubakaran <165383562+kirubakaran232@users.noreply.github.com>
2026-05-25 19:09:47 +0530

    Create 2831-NumberOfBeautifulPairs

---
+++ 2831-NumberOfBeautifulPairs/2831-NumberOfBeautifulPairs.java
@@ -0,0 +1,15 @@
+// Last updated: 5/25/2026, 7:09:47 PM
+class Solution {
+    public int countBeautifulPairs(int[] nums) {
+        int cnt = 0;
+        for(int i=0; i<nums.length; i++){
+            for(int j=i+1; j<nums.length; j++){
+                if(gcd(Integer.parseInt(String.valueOf(nums[i]).charAt(0)+""),nums[j]%10) == 1) cnt++;
+            }
+        }
+        return cnt;
+    }
+    int gcd(int a, int b){
+        return b == 0 ? a : gcd(b,a%b);
+    }
+}
