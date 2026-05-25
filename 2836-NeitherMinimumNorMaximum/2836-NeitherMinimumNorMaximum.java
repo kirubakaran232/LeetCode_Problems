90bc841a0bc2e3e8ccd3259b5817063f2edbc429
Kirubakaran <165383562+kirubakaran232@users.noreply.github.com>
2026-05-25 19:09:45 +0530

    Create 2836-NeitherMinimumNorMaximum

---
+++ 2836-NeitherMinimumNorMaximum/2836-NeitherMinimumNorMaximum.java
@@ -0,0 +1,17 @@
+// Last updated: 5/25/2026, 7:09:45 PM
+class Solution {
+    public int findNonMinOrMax(int[] nums) {
+        if (nums.length <= 2) return -1;
+        int min = Integer.MAX_VALUE;
+        int max = Integer.MIN_VALUE;
+        for (int num : nums) {
+            min = Math.min(min, num);
+            max = Math.max(max, num);
+        }
+        for (int num : nums) {
+            if (num != min && num != max)
+                return num;
+        }
+        return -1;
+    }
+}
