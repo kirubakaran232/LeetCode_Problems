f563f1b05e661427eed1ec7b68175dc76c4d3598
Kirubakaran <165383562+kirubakaran232@users.noreply.github.com>
2026-05-26 00:01:10 +0530

    File updated at 5/26/2026, 12:01:09 AM

--- 112-PathSum/112-PathSum.java
+++ 112-PathSum/112-PathSum.java
@@ -1,31 +1,31 @@
-// Last updated: 5/25/2026, 7:21:57 PM
-/**
- * Definition for a binary tree node.
- * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode() {}
- *     TreeNode(int val) { this.val = val; }
- *     TreeNode(int val, TreeNode left, TreeNode right) {
- *         this.val = val;
- *         this.left = left;
- *         this.right = right;
- *     }
- * }
- */
-class Solution{
-    static boolean dfs(TreeNode root,int sum,int target){
-        if(root==null){
-            return false;
-        }
-        if(root.left==null&&root.right==null){
-            return sum+root.val==target;
-        }
-        sum= sum+root.val;
-        return dfs(root.left,sum,target)||dfs(root.right,sum,target);
-    }
-    public boolean hasPathSum(TreeNode root,int targetSum){
-        return dfs(root,0,targetSum);
-    }
-}
+// Last updated: 5/26/2026, 12:01:09 AM
+1/**
+2 * Definition for a binary tree node.
+3 * public class TreeNode {
+4 *     int val;
+5 *     TreeNode left;
+6 *     TreeNode right;
+7 *     TreeNode() {}
+8 *     TreeNode(int val) { this.val = val; }
+9 *     TreeNode(int val, TreeNode left, TreeNode right) {
+10 *         this.val = val;
+11 *         this.left = left;
+12 *         this.right = right;
+13 *     }
+14 * }
+15 */
+16class Solution{
+17    static boolean dfs(TreeNode root,int sum,int target){
+18        if(root==null){
+19            return false;
+20        }
+21        if(root.left==null&&root.right==null){
+22            return sum+root.val==target;
+23        }
+24        sum= sum+root.val;
+25        return dfs(root.left,sum,target)||dfs(root.right,sum,target);
+26    }
+27    public boolean hasPathSum(TreeNode root,int targetSum){
+28        return dfs(root,0,targetSum);
+29    }
+30}
