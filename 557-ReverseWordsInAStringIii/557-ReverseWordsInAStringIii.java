20b598029eea5e7096ae49f0642a29d7073823d9
Kirubakaran <165383562+kirubakaran232@users.noreply.github.com>
2026-05-25 19:18:47 +0530

    Create 557-ReverseWordsInAStringIii

---
+++ 557-ReverseWordsInAStringIii/557-ReverseWordsInAStringIii.java
@@ -0,0 +1,16 @@
+// Last updated: 5/25/2026, 7:18:46 PM
+class Solution {
+    public String reverseWords(String s) {
+        // Split the input string into words and reverse each word
+        String[] words = s.split(" ");
+        StringBuilder result = new StringBuilder();
+        for (String word : words) {
+            StringBuilder reversedWord = new StringBuilder(word).reverse();
+            result.append(reversedWord).append(" ");
+        }
+        result.deleteCharAt(result.length() - 1);  // Remove the extra space at the end
+        
+        // Convert StringBuilder to String
+        return result.toString();        
+    }
+}
