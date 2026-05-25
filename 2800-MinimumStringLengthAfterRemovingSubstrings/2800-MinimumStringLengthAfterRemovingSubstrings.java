67afb2cf12e37f5f6e2edf2f036c2834b408e20f
Kirubakaran <165383562+kirubakaran232@users.noreply.github.com>
2026-05-25 19:09:55 +0530

    Create 2800-MinimumStringLengthAfterRemovingSubstrings

---
+++ 2800-MinimumStringLengthAfterRemovingSubstrings/2800-MinimumStringLengthAfterRemovingSubstrings.java
@@ -0,0 +1,14 @@
+// Last updated: 5/25/2026, 7:09:54 PM
+class Solution {
+    public int minLength(String s) {
+        Stack<Character> st = new Stack<>();
+        for(int i=0;i<s.length();i++){
+            if(!st.isEmpty() && ((st.peek()=='A' && s.charAt(i)=='B')||(st.peek()=='C' && s.charAt(i)=='D'))){
+                st.pop();
+            }else{
+                st.push(s.charAt(i));
+            }
+        }
+        return st.size();
+    }
+}
