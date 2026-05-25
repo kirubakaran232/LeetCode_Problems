14d56b523f3bbf452b516ba158b971d5bdb9972a
Kirubakaran <165383562+kirubakaran232@users.noreply.github.com>
2026-05-25 19:16:05 +0530

    Create 1341-SplitAStringInBalancedStrings

---
+++ 1341-SplitAStringInBalancedStrings/1341-SplitAStringInBalancedStrings.java
@@ -0,0 +1,15 @@
+// Last updated: 5/25/2026, 7:16:05 PM
+class Solution {
+    public int balancedStringSplit(String s) {
+        int count = 0;
+        int balance = 0;
+    
+        for(int i=0; i<s.length(); i++){
+            if(s.charAt(i) == 'R') balance++;
+            else balance--;
+
+            if(balance == 0) count++;
+        }
+        return count;
+    }
+}
