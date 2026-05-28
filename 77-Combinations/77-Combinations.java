9ebda85d9b3c2935a17734df9110a20b3c15417a
Kirubakaran <165383562+kirubakaran232@users.noreply.github.com>
2026-05-29 03:18:16 +0530

    File updated at 5/29/2026, 3:18:16 AM

--- 77-Combinations/77-Combinations.java
+++ 77-Combinations/77-Combinations.java
@@ -1,20 +1,20 @@
-// Last updated: 5/25/2026, 7:22:30 PM
-class Solution {
-    public List<List<Integer>> combine(int n, int k) {
-        List<List<Integer>> res = new ArrayList<>();
-        List<Integer> l = new ArrayList<>();
-        backtrack(1,res,l,n,k);
-        return res;
-    }
-    public void backtrack(int st,List<List<Integer>> res,List<Integer> l,int n,int k){
-        if(l.size()==k){
-            res.add(new ArrayList<>(l));
-            return;
-        }
-        for(int i=st;i<=n;i++){
-            l.add(i);
-            backtrack(i+1,res,l,n,k);
-            l.remove(l.size()-1);
-        }
-    }
-}
+// Last updated: 5/29/2026, 3:18:16 AM
+1class Solution {
+2    public List<List<Integer>> combine(int n, int k) {
+3        List<List<Integer>> res = new ArrayList<>();
+4        List<Integer> l = new ArrayList<>();
+5        backtrack(1,res,l,n,k);
+6        return res;
+7    }
+8    public void backtrack(int st,List<List<Integer>> res,List<Integer> l,int n,int k){
+9        if(l.size()==k){
+10            res.add(new ArrayList<>(l));
+11            return;
+12        }
+13        for(int i=st;i<=n;i++){
+14            l.add(i);
+15            backtrack(i+1,res,l,n,k);
+16            l.remove(l.size()-1);
+17        }
+18    }
+19}
