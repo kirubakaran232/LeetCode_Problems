a4c7eaa198a174a11e399cbb3ffa59ec1de3c271
Kirubakaran <165383562+kirubakaran232@users.noreply.github.com>
2026-05-29 03:17:21 +0530

    File updated at 5/29/2026, 3:17:21 AM

--- 49-GroupAnagrams/49-GroupAnagrams.java
+++ 49-GroupAnagrams/49-GroupAnagrams.java
@@ -1,14 +1,14 @@
-// Last updated: 5/25/2026, 7:23:05 PM
-class Solution {
-    public List<List<String>> groupAnagrams(String[] strs) {
-        Map<String, List<String>> hm = new HashMap<>();
-        for (int i = 0; i < strs.length; i++) {
-            char[] c = strs[i].toCharArray();
-            Arrays.sort(c);
-            String s = new String(c);
-            hm.putIfAbsent(s, new ArrayList<>());
-            hm.get(s).add(strs[i]);
-        }
-        return new ArrayList<>(hm.values());
-    }
-}
+// Last updated: 5/29/2026, 3:17:21 AM
+1class Solution {
+2    public List<List<String>> groupAnagrams(String[] strs) {
+3        Map<String, List<String>> hm = new HashMap<>();
+4        for (int i = 0; i < strs.length; i++) {
+5            char[] c = strs[i].toCharArray();
+6            Arrays.sort(c);
+7            String s = new String(c);
+8            hm.putIfAbsent(s, new ArrayList<>());
+9            hm.get(s).add(strs[i]);
+10        }
+11        return new ArrayList<>(hm.values());
+12    }
+13}
