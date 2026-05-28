f541821d78d3fc98766102282ea4776fb4f38d99
Kirubakaran <165383562+kirubakaran232@users.noreply.github.com>
2026-05-29 03:17:53 +0530

    File updated at 5/29/2026, 3:17:53 AM

--- 46-Permutations/46-Permutations.java
+++ 46-Permutations/46-Permutations.java
@@ -1,19 +1,19 @@
-// Last updated: 5/25/2026, 7:23:09 PM
-class Solution {
-    public List<List<Integer>> permute(int[] nums) {
-        List<List<Integer>> res = new ArrayList<>();
-        backtrack(res,new ArrayList<>(),nums);
-        return res;
-    }
-    public void backtrack(List<List<Integer>> res,List<Integer> l,int nums[]){
-        if(l.size()==nums.length) res.add(new ArrayList<>(l));
-        else{
-            for(int i=0;i<nums.length;i++){
-                if(l.contains(nums[i])) continue;
-                l.add(nums[i]);
-                backtrack(res,l,nums);
-                l.remove(l.size()-1);
-            }
-        }
-    }
-}
+// Last updated: 5/29/2026, 3:17:53 AM
+1class Solution {
+2    public List<List<Integer>> permute(int[] nums) {
+3        List<List<Integer>> res = new ArrayList<>();
+4        backtrack(res,new ArrayList<>(),nums);
+5        return res;
+6    }
+7    public void backtrack(List<List<Integer>> res,List<Integer> l,int nums[]){
+8        if(l.size()==nums.length) res.add(new ArrayList<>(l));
+9        else{
+10            for(int i=0;i<nums.length;i++){
+11                if(l.contains(nums[i])) continue;
+12                l.add(nums[i]);
+13                backtrack(res,l,nums);
+14                l.remove(l.size()-1);
+15            }
+16        }
+17    }
+18}
