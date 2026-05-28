aa955aa05f93bddba653ebb3fdeba07a310d0f67
Kirubakaran <165383562+kirubakaran232@users.noreply.github.com>
2026-05-28 16:03:16 +0530

    File updated at 5/28/2026, 4:03:16 PM

--- 424-LongestRepeatingCharacterReplacement/424-LongestRepeatingCharacterReplacement.java
+++ 424-LongestRepeatingCharacterReplacement/424-LongestRepeatingCharacterReplacement.java
@@ -1,20 +1,18 @@
-// Last updated: 5/25/2026, 7:19:26 PM
-class Solution {
-    public int characterReplacement(String s, int k) {
-        Map<Character,Integer> hm = new HashMap<>();
-        int l = 0;
-        int maxfreq = 0;
-        int maxlen = 0;
-        for(int r = 0; r < s.length(); r++){
-            char ch = s.charAt(r);
-            hm.put(ch, hm.getOrDefault(ch, 0) + 1);
-            maxfreq = Math.max(maxfreq, hm.get(ch));
-            while((r - l + 1) - maxfreq > k){
-                hm.put(s.charAt(l), hm.get(s.charAt(l)) - 1);
-                l++;
-            }
-            maxlen = Math.max(maxlen, r - l + 1);
-        }
-        return maxlen;
-    }
-}
+// Last updated: 5/28/2026, 4:03:16 PM
+1class Solution {
+2    public int characterReplacement(String s, int k) {
+3        int l = 0,mf = 0,ml = 0;
+4        Map<Character,Integer> hm = new HashMap<>();
+5        for(int r=0;r<s.length();r++){
+6            char ch = s.charAt(r);
+7            hm.put(ch,hm.getOrDefault(ch,0)+1);
+8            mf = Math.max(mf,hm.get(ch));
+9            while((r-l+1)-mf>k){
+10                hm.put(s.charAt(l),hm.get(s.charAt(l))-1);
+11                l++;
+12            }
+13            ml = Math.max(ml,(r-l+1));
+14        }
+15        return ml;
+16    }
+17}
