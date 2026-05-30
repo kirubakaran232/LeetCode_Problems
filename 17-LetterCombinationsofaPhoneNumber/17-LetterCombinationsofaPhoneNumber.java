e492b9b5359fdba371ef0a7d37e0e79ef91f106c
Kirubakaran <165383562+kirubakaran232@users.noreply.github.com>
2026-05-30 07:06:55 +0530

    File updated at 5/30/2026, 7:06:54 AM

--- 17-LetterCombinationsofaPhoneNumber/17-LetterCombinationsofaPhoneNumber.java
+++ 17-LetterCombinationsofaPhoneNumber/17-LetterCombinationsofaPhoneNumber.java
@@ -1,32 +1,31 @@
-// Last updated: 5/30/2026, 7:06:23 AM
+// Last updated: 5/30/2026, 7:06:54 AM
 1class Solution {
-2    Map<Character,String> hm;
+2    HashMap<Character,String> hm;
 3    List<String> res;
-4    public List<String> letterCombinations(String digits) {
-5        int n = digits.length();
-6        hm = new HashMap<>();
-7        res = new ArrayList<>();
-8        hm.put('2',"abc");
-9        hm.put('3',"def");
-10        hm.put('4',"ghi");
-11        hm.put('5',"jkl");
-12        hm.put('6',"mno");
-13        hm.put('7',"pqrs");
-14        hm.put('8',"tuv");
-15        hm.put('9',"wxyz");
-16        backtrack(digits,new StringBuilder(),0);
-17        return res;
-18    }
-19    public void backtrack(String dig,StringBuilder path,int ind){
-20        if (ind == dig.length()) {
-21            res.add(path.toString());
-22            return;
-23        }
-24        String x = hm.get(dig.charAt(ind));
-25        for (char i : x.toCharArray()) {
-26            path.append(i);
-27            backtrack(dig,path,ind+1);
-28            path.deleteCharAt(path.length() - 1);
-29        }
-30    }
-31}
+4    public List<String> letterCombinations(String d) {
+5        hm = new HashMap<>();
+6        res = new ArrayList<>();
+7        hm.put('2',"abc");
+8        hm.put('3',"def");
+9        hm.put('4',"ghi");
+10        hm.put('5',"jkl");
+11        hm.put('6',"mno");
+12        hm.put('7',"pqrs");
+13        hm.put('8',"tuv");
+14        hm.put('9',"wxyz");
+15        bt(d,new StringBuilder(),0);
+16        return res;
+17    }
+18    public void bt(String d, StringBuilder sb, int ind){
+19        if(ind == d.length()){
+20            res.add(sb.toString());
+21            return;
+22        }
+23        String s = hm.get(d.charAt(ind));
+24        for(int i=0; i<s.length(); i++){
+25            sb.append(s.charAt(i));
+26            bt(d, sb, ind+1);
+27            sb.deleteCharAt(sb.length()-1);
+28        }
+29    }
+30}
