61537d11675961d857844c95a99f655337ede716
Kirubakaran <165383562+kirubakaran232@users.noreply.github.com>
2026-06-01 10:13:38 +0530

    File updated at 6/1/2026, 10:13:38 AM

--- 190-ReverseBits/190-ReverseBits.java
+++ 190-ReverseBits/190-ReverseBits.java
@@ -1,12 +1,12 @@
-// Last updated: 5/25/2026, 7:21:06 PM
-class Solution {
-    public int reverseBits(int n) {
-        int sum=0;
-        for(int i=0;i<32;i++){
-            sum=sum<<1;
-            sum+=(n&1);
-            n=n>>1;
-        }
-        return sum;
-    }
-}
+// Last updated: 6/1/2026, 10:13:38 AM
+1class Solution {
+2    public int reverseBits(int n) {
+3        int sum = 0;
+4        for(int i=0;i<32;i++){
+5            sum = sum<<1;
+6            sum = sum + (n&1);
+7            n=n>>1;
+8        }
+9        return sum;
+10    }
+11}
