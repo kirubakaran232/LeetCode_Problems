c76360341f45e1625a3f3bd17cd70c8b65a43b4f
Kirubakaran <165383562+kirubakaran232@users.noreply.github.com>
2026-06-02 21:13:22 +0530

    File updated at 6/2/2026, 9:13:21 PM

--- 69-Sqrt(x)/69-Sqrt(x).java
+++ 69-Sqrt(x)/69-Sqrt(x).java
@@ -1,20 +1,19 @@
-// Last updated: 5/26/2026, 9:52:19 PM
+// Last updated: 6/2/2026, 9:13:21 PM
 1class Solution {
 2    public int mySqrt(int x) {
-3        if (x < 2) return x;
-4        int left = 1, right = x / 2;
-5        while (left <= right) {
-6            int mid = left + (right - left) / 2;
-7            long sq = (long) mid * mid;
-8            if (sq == x) {
+3        if(x<2) return x;
+4        int l = 1,r = x/2;
+5        while(l<=r){
+6            int mid = (l+r)/2;
+7            long sq = (long) mid*mid;
+8            if(sq == x){
 9                return mid;
-10            } else if (sq > x) {
-11                right = mid - 1;
-12            } else {
-13                left = mid + 1;
+10            }else if(sq>x){
+11                r = mid-1;
+12            }else{
+13                l = mid+1;
 14            }
 15        }
-16        return right;   
+16        return r;
 17    }
 18}
-19
