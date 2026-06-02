87c36435a7869fefbeea34f2c21d5392619a50af
Kirubakaran <165383562+kirubakaran232@users.noreply.github.com>
2026-06-02 22:04:24 +0530

    File updated at 6/2/2026, 10:04:23 PM

--- 221-MaximalSquare/221-MaximalSquare.java
+++ 221-MaximalSquare/221-MaximalSquare.java
@@ -1,18 +1,18 @@
-// Last updated: 5/25/2026, 7:20:39 PM
-class Solution {
-    public int maximalSquare(char[][] matrix) {
-        int rows = matrix.length;
-        int cols = matrix[0].length;
-        int max = 0;
-        int [][] dp = new int [rows+1][cols+1];
-        for (int i = 1; i <= rows; i++) {
-            for (int j = 1; j <= cols; j++) {
-                if (matrix[i-1][j-1] == '1') {
-                    dp[i][j] = 1 + Math.min(Math.min(dp[i-1][j], dp[i][j-1]),dp[i-1][j-1]);
-                }
-                max = Math.max(dp[i][j], max);
-            }
-        }
-        return max * max;
-    }
-}
+// Last updated: 6/2/2026, 10:04:23 PM
+1class Solution {
+2    public int maximalSquare(char[][] matrix) {
+3        int rows = matrix.length;
+4        int cols = matrix[0].length;
+5        int max = 0;
+6        int [][] dp = new int [rows+1][cols+1];
+7        for (int i = 1; i <= rows; i++) {
+8            for (int j = 1; j <= cols; j++) {
+9                if (matrix[i-1][j-1] == '1') {
+10                    dp[i][j] = 1 + Math.min(Math.min(dp[i-1][j], dp[i][j-1]),dp[i-1][j-1]);
+11                }
+12                max = Math.max(dp[i][j], max);
+13            }
+14        }
+15        return max * max;
+16    }
+17}
