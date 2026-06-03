7e25b15f15eacf7595c6fbb18de11b0da81239c7
Kirubakaran <165383562+kirubakaran232@users.noreply.github.com>
2026-06-04 00:45:32 +0530

    File updated at 6/4/2026, 12:45:32 AM

--- 5-LongestPalindromicSubstring/5-LongestPalindromicSubstring.java
+++ 5-LongestPalindromicSubstring/5-LongestPalindromicSubstring.java
@@ -1,29 +1,29 @@
-// Last updated: 5/25/2026, 7:23:52 PM
-class Solution {
-    public String longestPalindrome(String s) {
-        int n = s.length();
-        boolean dp[][] = new boolean[n][n];
-        int max = 0;
-        String res = "";
-        for(int i=0;i<n;i++){
-            dp[i][i]=true;
-            res = s.substring(i, i + 1);
-            max = 1;
-        }
-        for(int len=2;len<=n;len++){
-            for(int i=0;i<=n-len;i++){
-                int j = i+len-1;
-                if(s.charAt(i)==s.charAt(j)){
-                    if (len == 2 || dp[i + 1][j - 1]) {
-                        dp[i][j]=true;
-                        if(len>max){
-                            res = s.substring(i,j+1);
-                            max = len;
-                        }
-                    }
-                }
-            }
-        }
-        return res;
-    }
-}
+// Last updated: 6/4/2026, 12:45:32 AM
+1class Solution {
+2    public String longestPalindrome(String s) {
+3        int n = s.length();
+4        boolean dp[][] = new boolean[n][n];
+5        int max = 0;
+6        String res = "";
+7        for(int i=0;i<n;i++){
+8            dp[i][i]=true;
+9            res = s.substring(i, i + 1);
+10            max = 1;
+11        }
+12        for(int len=2;len<=n;len++){
+13            for(int i=0;i<=n-len;i++){
+14                int j = i+len-1;
+15                if(s.charAt(i)==s.charAt(j)){
+16                    if (len == 2 || dp[i + 1][j - 1]) {
+17                        dp[i][j]=true;
+18                        if(len>max){
+19                            res = s.substring(i,j+1);
+20                            max = len;
+21                        }
+22                    }
+23                }
+24            }
+25        }
+26        return res;
+27    }
+28}
