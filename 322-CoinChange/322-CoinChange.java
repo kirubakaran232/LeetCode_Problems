dfaf43cde5a3cca01212574bebfd4b5e3b03a4be
Kirubakaran <165383562+kirubakaran232@users.noreply.github.com>
2026-06-04 22:38:45 +0530

    File updated at 6/4/2026, 10:38:45 PM

--- 322-CoinChange/322-CoinChange.java
+++ 322-CoinChange/322-CoinChange.java
@@ -1,23 +1,23 @@
-// Last updated: 5/25/2026, 7:19:53 PM
-class Solution {
-    public int coinChange(int[] coins, int amount) {
-        int dp[][] = new int[coins.length+1][amount+1];
-        for (int j = 1; j <= amount; j++) {
-            dp[0][j] = amount+1; 
-        }
-        for (int i = 1; i <= coins.length; i++) {
-            dp[i][0] = 0; 
-        }
-        for(int i=1;i<=coins.length;i++){
-            for(int j=1;j<=amount;j++){
-                if(coins[i-1]>j){
-                    dp[i][j]=dp[i-1][j];
-                }else{
-                    dp[i][j]=Math.min(dp[i][j-coins[i-1]]+1,dp[i-1][j]);
-                }
-                
-            }
-        }
-        return dp[coins.length][amount] > amount ? -1 : dp[coins.length][amount];
-    }
-}
+// Last updated: 6/4/2026, 10:38:45 PM
+1class Solution {
+2    public int coinChange(int[] coins, int amount) {
+3        int dp[][] = new int[coins.length+1][amount+1];
+4        for (int j = 1; j <= amount; j++) {
+5            dp[0][j] = amount+1; 
+6        }
+7        for (int i = 1; i <= coins.length; i++) {
+8            dp[i][0] = 0; 
+9        }
+10        for(int i=1;i<=coins.length;i++){
+11            for(int j=1;j<=amount;j++){
+12                if(coins[i-1]>j){
+13                    dp[i][j]=dp[i-1][j];
+14                }else{
+15                    dp[i][j]=Math.min(dp[i][j-coins[i-1]]+1,dp[i-1][j]);
+16                }
+17                
+18            }
+19        }
+20        return dp[coins.length][amount] > amount ? -1 : dp[coins.length][amount];
+21    }
+22}
