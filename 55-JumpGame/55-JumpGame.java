e247f687d3ec7891f3973466130e11bf4520d6d3
Kirubakaran <165383562+kirubakaran232@users.noreply.github.com>
2026-06-05 06:57:13 +0530

    File updated at 6/5/2026, 6:57:13 AM

--- 55-JumpGame/55-JumpGame.java
+++ 55-JumpGame/55-JumpGame.java
@@ -1,12 +1,12 @@
-// Last updated: 5/25/2026, 7:22:57 PM
-class Solution {
-    public boolean canJump(int[] nums) {
-        int max = 0;
-        int n = nums.length;
-        for(int i=0;i<nums.length;i++){
-            if(max<i)return false;
-            max = Math.max(nums[i]+i,max);
-        }
-        return true;
-    }
-}
+// Last updated: 6/5/2026, 6:57:13 AM
+1class Solution {
+2    public boolean canJump(int[] nums) {
+3        int max = 0;
+4        int n = nums.length;
+5        for(int i=0;i<nums.length;i++){
+6            if(max<i)return false;
+7            max = Math.max(nums[i]+i,max);
+8        }
+9        return true;
+10    }
+11}
