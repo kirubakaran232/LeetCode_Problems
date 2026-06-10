aa5655a6959449c339b0d2afc013cd6b55ab820c
Kirubakaran <165383562+kirubakaran232@users.noreply.github.com>
2026-06-10 23:35:01 +0530

    File updated at 6/10/2026, 11:35:01 PM

--- 6-ZigzagConversion/6-ZigzagConversion.java
+++ 6-ZigzagConversion/6-ZigzagConversion.java
@@ -1,26 +1,26 @@
-// Last updated: 5/25/2026, 7:23:51 PM
-class Solution {
-    public String convert(String s, int n) {
-        if (n == 1 || s.length() <= n)
-            return s;
-        StringBuilder sb[] = new StringBuilder[n];
-        for (int i = 0; i < n; i++) {
-            sb[i] = new StringBuilder();
-        }
-        int dir = 1, r = 0;
-        for (int i = 0; i < s.length(); i++) {
-            char ch = s.charAt(i);
-            sb[r].append(ch);
-            if (r == 0)
-                dir = 1;
-            else if (r == n - 1)
-                dir = -1;
-            r += dir;
-        }
-        StringBuilder res = new StringBuilder();
-        for (int i = 0; i < n; i++) {
-            res.append(sb[i]);
-        }
-        return res.toString();
-    }
-}
+// Last updated: 6/10/2026, 11:35:01 PM
+1class Solution {
+2    public String convert(String s, int n) {
+3        if (n == 1 || s.length() <= n)
+4            return s;
+5        StringBuilder sb[] = new StringBuilder[n];
+6        for (int i = 0; i < n; i++) {
+7            sb[i] = new StringBuilder();
+8        }
+9        int dir = 1, r = 0;
+10        for (int i = 0; i < s.length(); i++) {
+11            char ch = s.charAt(i);
+12            sb[r].append(ch);
+13            if (r == 0)
+14                dir = 1;
+15            else if (r == n - 1)
+16                dir = -1;
+17            r += dir;
+18        }
+19        StringBuilder res = new StringBuilder();
+20        for (int i = 0; i < n; i++) {
+21            res.append(sb[i]);
+22        }
+23        return res.toString();
+24    }
+25}
