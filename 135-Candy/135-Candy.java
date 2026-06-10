c43a7603dee1564b2110f08716839dd96cb62f45
Kirubakaran <165383562+kirubakaran232@users.noreply.github.com>
2026-06-10 23:05:55 +0530

    File updated at 6/10/2026, 11:05:54 PM

--- 135-Candy/135-Candy.java
+++ 135-Candy/135-Candy.java
@@ -1,21 +1,21 @@
-// Last updated: 5/25/2026, 7:21:43 PM
-class Solution {
-    public int candy(int[] ratings) {
-    int n = ratings.length;
-    int[] candies = new int[n];
-    Arrays.fill(candies, 1);
-    for (int i = 1; i < n; i++) {
-        if (ratings[i] > ratings[i - 1]) {
-            candies[i] = candies[i - 1] + 1;
-        }
-    }
-    for (int i = n - 2; i >= 0; i--) {
-        if (ratings[i] > ratings[i + 1]) {
-            candies[i] = Math.max(candies[i], candies[i + 1] + 1);
-        }
-    }
-    int total = 0;
-    for (int c : candies) total += c;
-        return total;
-    }
-}
+// Last updated: 6/10/2026, 11:05:54 PM
+1class Solution {
+2    public int candy(int[] ratings) {
+3    int n = ratings.length;
+4    int[] candies = new int[n];
+5    Arrays.fill(candies, 1);
+6    for (int i = 1; i < n; i++) {
+7        if (ratings[i] > ratings[i - 1]) {
+8            candies[i] = candies[i - 1] + 1;
+9        }
+10    }
+11    for (int i = n - 2; i >= 0; i--) {
+12        if (ratings[i] > ratings[i + 1]) {
+13            candies[i] = Math.max(candies[i], candies[i + 1] + 1);
+14        }
+15    }
+16    int tot = 0;
+17    for (int i=0;i<candies.length;i++) tot += candies[i];
+18    return tot;
+19    }
+20}
