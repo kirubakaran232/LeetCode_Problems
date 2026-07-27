059b1b8bfc53980f79880e5514ab82228cb5677d
Kirubakaran <165383562+kirubakaran232@users.noreply.github.com>
2026-07-27 12:24:14 +0530

    File updated at 7/27/2026, 12:24:14 PM

--- 73-SetMatrixZeroes/73-SetMatrixZeroes.java
+++ 73-SetMatrixZeroes/73-SetMatrixZeroes.java
@@ -1,44 +1,24 @@
-// Last updated: 5/25/2026, 7:22:37 PM
+// Last updated: 7/27/2026, 12:24:14 PM
 class Solution {
-    public void setZeroes(int[][] mat) {
-        boolean fr = false, fc = false;
-        for (int i = 0; i < mat[0].length; i++) {
-            if (mat[0][i] == 0) {
-                fr = true;
-                break;
-            }
-        }
-        for (int i = 0; i < mat.length; i++) {
-            if (mat[i][0] == 0) {
-                fc = true;
-                break;
-            }
-        }
-        for (int i = 1; i < mat.length; i++) {
-            for (int j = 1; j < mat[0].length; j++) {
-                if (mat[i][j] == 0) {
-                    mat[i][0] = 0;
-                    mat[0][j] = 0;
+    public void setZeroes(int[][] matrix) {
+        int m = matrix.length;
+        int n = matrix[0].length;
+        boolean[] row = new boolean[m];
+        boolean[] col = new boolean[n];
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (matrix[i][j] == 0) {
+                    row[i] = true;
+                    col[j] = true;
                 }
             }
         }
-        for (int i = 1; i < mat.length; i++) {
-            for (int j = 1; j < mat[0].length; j++) {
-                if (mat[i][0] == 0 || mat[0][j] == 0) {
-                    mat[i][j] = 0;
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (row[i] || col[j]) {
+                    matrix[i][j] = 0;
                 }
             }
         }
-        if (fc) {
-            for (int i = 0; i < mat.length; i++) {
-                mat[i][0] = 0;
-            }
-        }
-        if (fr) {
-            for (int j = 0; j < mat[0].length; j++) {
-                mat[0][j] = 0;
-            }
-        }
-
     }
 }
