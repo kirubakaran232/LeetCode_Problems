0fdc5e6ee10a1e102016ca3ac54517c8aea83328
Kirubakaran <165383562+kirubakaran232@users.noreply.github.com>
2026-07-27 21:21:09 +0530

    File updated at 7/27/2026, 9:21:08 PM

--- 11-ContainerWithMostWater/11-ContainerWithMostWater.java
+++ 11-ContainerWithMostWater/11-ContainerWithMostWater.java
@@ -1,21 +1,19 @@
-// Last updated: 5/29/2026, 3:17:04 AM
+// Last updated: 7/27/2026, 9:21:08 PM
 1class Solution {
 2    public int maxArea(int[] h) {
 3        int l = 0,r = h.length-1;
 4        int area = 0;
 5        while(l<r){
-6            if(h[l]<h[r]){
-7                int ht = Math.min(h[l],h[r]);
-8                int w = r-l;
+6            int ht = Math.min(h[l],h[r]);
+7            int w = r-l;
+8            if(h[l]<h[r]){
 9                area = Math.max(area,ht*w);
 10                l++;
 11            }else{
-12                int ht = Math.min(h[l],h[r]);
-13                int w = r-l;
-14                area = Math.max(area,ht*w);
-15                r--;
-16            }
-17        }
-18        return area;
-19    }
-20}
+12                area = Math.max(area,ht*w);
+13                r--;
+14            }
+15        }
+16        return area;
+17    }
+18}
