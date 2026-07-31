0e6ca2a89fefc0a9dca15ad630b9a2981828a084
Kirubakaran <165383562+kirubakaran232@users.noreply.github.com>
2026-07-31 15:46:17 +0530

    File updated at 7/31/2026, 3:46:17 PM

--- 39-CombinationSum/39-CombinationSum.java
+++ 39-CombinationSum/39-CombinationSum.java
@@ -1,22 +1,23 @@
-// Last updated: 6/30/2026, 9:50:45 AM
+// Last updated: 7/31/2026, 3:46:17 PM
 1class Solution {
-2    public List<List<Integer>> combinationSum(int[] arr, int t) {
-3        ArrayList<List<Integer>> res = new ArrayList<>();
-4        Arrays.sort(arr);
-5        bt(arr,new ArrayList<>(),res,0,t);
-6        return res;
-7    }
-8    static void bt(int arr[],ArrayList<Integer> l,ArrayList<List<Integer>> res,int st,int rem){
-9        if(rem<0) return;
-10        else if(rem==0){
-11            res.add(new ArrayList<Integer>(l));
-12            return;
-13        }else{
-14            for(int i=st;i<arr.length;i++){
-15                if(rem>0) l.add(arr[i]);
-16                bt(arr,l,res,i,rem-arr[i]);
-17                l.remove(l.size()-1);
-18            }
-19        }
-20    }
-21}
+2    public List<List<Integer>> combinationSum(int[] c, int k) {
+3        List<List<Integer>> res = new ArrayList<>();
+4        bt(res,new ArrayList<>(),c,k,0,0);
+5        return res;
+6    }
+7    public void bt(List<List<Integer>> res,List<Integer> l,int[] c,int k,int sum,int s){
+8        if(sum==k){
+9            res.add(new ArrayList<>(l));
+10            s++;
+11            return;
+12        }
+13        else if(sum>k) return;
+14        for(int i=s;i<c.length;i++){
+15            l.add(c[i]);
+16            sum+=c[i];
+17            bt(res,l,c,k,sum,i);
+18            l.remove(l.size()-1);
+19            sum-=c[i];
+20        }
+21    }
+22}
