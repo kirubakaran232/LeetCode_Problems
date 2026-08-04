40933b43098ac3eff967fd4dbf403e73ae5ff615
Kirubakaran <165383562+kirubakaran232@users.noreply.github.com>
2026-08-04 18:20:50 +0530

    File updated at 8/4/2026, 6:20:50 PM

--- 22-GenerateParentheses/22-GenerateParentheses.java
+++ 22-GenerateParentheses/22-GenerateParentheses.java
@@ -1,18 +1,19 @@
-// Last updated: 5/26/2026, 12:10:03 AM
+// Last updated: 8/4/2026, 6:20:50 PM
 1class Solution {
-2    void generate(List<String> ans, String s, int open, int close, int n) {
-3        if (open == n && close == n) {
-4            ans.add(s);
-5            return;
-6        }
-7        if (open > close)
-8            generate(ans, s + ")", open, close + 1, n);
-9        if (open < n)
-10            generate(ans, s + "(", open + 1, close, n);
-11    }
-12    public List<String> generateParenthesis(int n) {
-13        List<String> ans = new ArrayList<>();
-14        generate(ans, "", 0, 0, n);
-15        return ans;
-16    }
-17}
+2    public List<String> generateParenthesis(int n) {
+3        List<String> res = new ArrayList<>();
+4        bt(res,0,0,n,"");
+5        return res;
+6    }
+7    public void bt(List<String> res,int op,int cl,int n,String s){
+8        if(op==n && cl==n){
+9            res.add(s);
+10        }
+11        if(op<n){
+12            bt(res,op+1,cl,n,s+"(");
+13        }
+14        if(cl<op){
+15            bt(res,op,cl+1,n,s+")");
+16        }
+17    }
+18}
