4937bd5f0c1888a47f04c7dcc6aa5ea057ccb097
Kirubakaran <165383562+kirubakaran232@users.noreply.github.com>
2026-08-21 15:53:36 +0530

    File updated at 8/21/2026, 3:53:36 PM

--- 2452-WordsWithinTwoEditsofDictionary/2452-WordsWithinTwoEditsofDictionary.java
+++ 2452-WordsWithinTwoEditsofDictionary/2452-WordsWithinTwoEditsofDictionary.java
@@ -1,22 +1,24 @@
-// Last updated: 8/21/2026, 3:49:10 PM
+// Last updated: 8/21/2026, 3:53:36 PM
 1class Solution {
 2    public List<String> twoEditWords(String[] q, String[] d) {
 3        List<String> l = new ArrayList<>();
 4        for(String s:q){
 5            for(String st:d){
-6                int dif = 0;
-7                for(int i=0;i<s.length();i++){
-8                    if(s.charAt(i)!=st.charAt(i)) dif++;
-9                    if(dif>2){
-10                        break;
-11                    }
-12                }
-13                if (dif <= 2) {
-14                    l.add(s);
-15                    break;
-16                }
-17            }
-18        }
-19        return l;
-20    }
-21}
+6                Boolean b = false;
+7                int dif = 0;
+8                for(int i=0;i<s.length();i++){
+9                    if(s.charAt(i)!=st.charAt(i)) dif++;
+10                    if(dif>2){
+11                        b = true;
+12                        break;
+13                    }
+14                }
+15                if(!b){
+16                    l.add(s);
+17                    break;
+18                }
+19            }
+20        }
+21        return l;
+22    }
+23}
