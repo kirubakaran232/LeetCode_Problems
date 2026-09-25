ead3ed1f82068f3188152cb781d4ab8dfb221f32
Kirubakaran <165383562+kirubakaran232@users.noreply.github.com>
2026-09-25 14:27:14 +0530

    File updated at 9/25/2026, 2:27:15 PM

--- 1312-MinimumInsertionStepstoMakeaStringPalindrome/1312-MinimumInsertionStepstoMakeaStringPalindrome.java
+++ 1312-MinimumInsertionStepstoMakeaStringPalindrome/1312-MinimumInsertionStepstoMakeaStringPalindrome.java
@@ -1,18 +1,24 @@
-// Last updated: 6/13/2026, 11:17:16 PM
+// Last updated: 9/25/2026, 2:27:15 PM
 1class Solution {
 2    public int minInsertions(String s) {
 3        int n = s.length();
-4        String rev = new StringBuilder(s).reverse().toString();
-5        int dp[][] = new int[n+1][n+1];
-6        for(int i=1;i<=n;i++){
-7            for(int j=1;j<=n;j++){
-8                if(s.charAt(i-1)==rev.charAt(j-1)){
-9                    dp[i][j] = 1+dp[i-1][j-1];
-10                }else{
-11                    dp[i][j] = Math.max(dp[i-1][j] , dp[i][j-1]);
-12                }
-13            }
-14        }
-15        return s.length() - dp[n][n];
-16    }
-17}
+4        int dp[][] = new int[n][n];
+5        for(int i=0;i<n;i++){
+6            dp[i][i] = 1;
+7        }
+8        for(int l=2;l<=n;l++){
+9            for(int i=0;i<=n-l;i++){
+10                int j = i+l-1;
+11                if(s.charAt(i)==s.charAt(j)){
+12                    if(l==2){
+13                        dp[i][j] = 2;
+14                    }else
+15                        dp[i][j] = 2+dp[i+1][j-1];
+16                }else{
+17                    dp[i][j] = Math.max(dp[i+1][j],dp[i][j-1]);
+18                }
+19            }
+20        }
+21        return n-dp[0][n-1];
+22    }
+23}
